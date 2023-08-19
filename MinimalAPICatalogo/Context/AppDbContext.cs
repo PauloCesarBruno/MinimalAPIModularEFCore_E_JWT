3d9f518094e9d317e2657a83f4b056420d61a485
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-08-18 21:24:39 -0300

    Add project files.

---
+++ MinimalAPICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPICatalogo.Models;
+
+namespace MinimalAPICatalogo.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+    { }
+
+    public DbSet <Produto> Produtos { get; set; }
+    public DbSet <Categoria> Categorias { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Uso de Fluent API
+
+        //Categoria:
+        modelBuilder.Entity<Categoria>().HasKey(ch => ch.CategoriaId);
+
+        modelBuilder.Entity<Categoria>().Property(n => n.Nome)
+                                        .HasMaxLength(100)
+                                        .IsRequired();
+
+        modelBuilder.Entity<Categoria>().Property(d => d.Descricao)
+                                       .HasMaxLength(250)
+                                       .IsRequired();
+        //
+
+        //Produto:
+        modelBuilder.Entity<Produto>().HasKey(ch => ch.ProdutoId);
+
+        modelBuilder.Entity<Produto>().Property(n => n.Nome)
+                                      .HasMaxLength(100)
+                                      .IsRequired();
+
+        modelBuilder.Entity<Produto>().Property(d => d.Descricao)
+                                      .HasMaxLength(250)
+                                      .IsRequired();
+
+        modelBuilder.Entity<Produto>().Property(i => i.Imagem)
+                                      .HasMaxLength(350)
+                                      .IsRequired();
+
+        modelBuilder.Entity<Produto>().Property(p => p.preco)
+                                       .HasPrecision(18, 2)
+                                       .IsRequired();
+        //
+
+        // Relacionamento:
+        modelBuilder.Entity<Produto>()
+                    .HasOne<Categoria>(c => c.Categoria)
+                    .WithMany(p => p.Produtos)
+                    .HasForeignKey(c => c.CategoriaId);
+    }
+}

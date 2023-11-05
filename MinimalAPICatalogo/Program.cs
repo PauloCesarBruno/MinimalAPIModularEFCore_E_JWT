c240b0b0d1389cf624550159b658b807f2b6f96e
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-11-05 02:23:16 -0300

    Finalizado

--- MinimalAPICatalogo/Program.cs
+++ MinimalAPICatalogo/Program.cs
@@ -3,7 +3,7 @@ using MinimalAPICatalogo.ServicesExtensionsApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Da pasta ServicesExtensionsApp e suas classes
+// Da pasta ServicesExtensionsApp e suas classes.
 builder.AddApiSwagger();
 builder.AddPersistence();
 builder.Services.AddCors();
@@ -11,13 +11,13 @@ builder.AddAutenticationJwt();
 
 var app = builder.Build();
 
-// Da pasta ModularEndPoints e sua classe ServicesCollectionExtensions
+// Da pasta ModularEndPoints e sua classe ServicesCollectionExtensions.
 app.MapAuenticacaoEndPoints();
 app.MapCategoriasPoints();
 app.MapProdutosEndPoints();
 
 
-// Da pasta ModularEndPoints e sua classeAppBuildeExtensions
+// Da pasta ModularEndPoints e sua classeAppBuildeExtensions. 
 var environment = app.Environment;
 app.UseExceptionHandling(environment)
    .UseSwaggerMidlleware()
